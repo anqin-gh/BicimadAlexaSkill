98a4ef12702ca09ee4b3c1943d732f7ec1a34978
Carlos Asensio <pizzicarlos89@gmail.com>
2024-11-02 20:14:16 +0100

    Add zod validation, exponential backoff and increase auth retries

--- src/helpers/authHelper.ts
+++ src/helpers/authHelper.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import {z} from 'zod';
 import {retry} from 'ts-retry-promise';
 import {inspect} from 'util';
 
@@ -6,25 +7,31 @@ const AUTH_URL = 'https://openapi.emtmadrid.es/v2/mobilitylabs/user/login/';
 const BICIMAD_X_CLIENT_ID = process.env.clientId;
 const BICIMAD_PASS_KEY = process.env.passKey;
 
-type BicimadAuthResponse = {
-  data: {
-    accessToken: string;
-  }[];
-};
+const bicimadAuthResponseSchema = z.object({
+  data: z.object({
+    data: z.array(
+      z.object({
+        accessToken: z.string(),
+      })
+    ),
+  }),
+});
 
 async function getAccessToken() {
   try {
     const {data} = await retry(
       () =>
-        axios.get<BicimadAuthResponse>(AUTH_URL, {
+        axios.get(AUTH_URL, {
           headers: {
             'X-ClientId': BICIMAD_X_CLIENT_ID,
             passKey: BICIMAD_PASS_KEY,
           },
         }),
-      {retries: 10}
-    );
-
+      {retries: 10, backoff: 'EXPONENTIAL'}
+    ).then(res => {
+      console.log(`Response: ${inspect(res)}`);
+      return bicimadAuthResponseSchema.parse(res);
+    });
     const accessToken = data.data[0].accessToken;
     return accessToken;
   } catch (err) {
--- src/helpers/bicimadHelper.ts
+++ src/helpers/bicimadHelper.ts
@@ -1,35 +1,41 @@
 import axios from 'axios';
 import getAccessToken from './authHelper';
-import {inspect} from 'util';
+import {z} from 'zod';
 import {retry} from 'ts-retry-promise';
+import {inspect} from 'util';
 
 const BICIMAD_STATIONS_URL =
   'https://openapi.emtmadrid.es/v1/transport/bicimad/stations/';
 export const BICIMAD_HOME_STATION_ID = 1576;
 export const BICIMAD_WORK_STATION_ID = 1524;
 
-type BicimadStationInformationResponse = {
-  data: {
-    dock_bikes: number;
-    free_bases: number;
-  }[];
-};
+const bicimadStationInformationResponseSchema = z.object({
+  data: z.object({
+    data: z.array(
+      z.object({
+        dock_bikes: z.number(),
+        free_bases: z.number(),
+      })
+    ),
+  }),
+});
 
 async function getInfoForStation(id: number) {
   try {
     const accessToken = await getAccessToken();
     const {data} = await retry(
       () =>
-        axios.get<BicimadStationInformationResponse>(
-          BICIMAD_STATIONS_URL + id,
-          {
-            headers: {
-              accessToken,
-            },
-          }
-        ),
-      {retries: 10}
-    );
+        axios.get(BICIMAD_STATIONS_URL + id, {
+          headers: {
+            accessToken,
+          },
+        }),
+      {retries: 12, backoff: 'EXPONENTIAL'}
+    ).then(res => {
+      console.log(`Response: ${inspect(res)}`);
+      return bicimadStationInformationResponseSchema.parse(res);
+    });
+
     return data.data[0];
   } catch (err) {
     console.error('Error while restrieving Bicimad info: ' + inspect(err));
